0297651f0046a3848562901ae8ea39447da6b129
Joao Carlos Duarte Martins Neto <netojn374741@gmail.com>
2025-01-25 22:22:36 -0300

    ok

--- SEÇÃO-02-Python intermediário/02-aula.py
+++ SEÇÃO-02-Python intermediário/02-aula.py
@@ -7,6 +7,9 @@
 # -> (Parte 2) Métodos úteis nos dicionários Python (dict)
 
 # -> Introdução ao tipo set em Python (conjuntos)
+# -> Peculiaridades do tipo mutável set em Python
+# -> Métodos úteis do tipo set em Python
+# -> Operadores importantes para o tipo set (conjuntos)
 
 
 
@@ -372,9 +375,206 @@ print(p1)
 
 # ================ Introdução ao tipo set em Python (conjuntos) ====================
 
+# Sets - Conjuntos em Python (tipo set)
+# Conjuntos são ensinados na matemática
+# https://brasilescola.uol.com.br/matematica/conjunto.htm
+# Representados graficamente pelo diagrama de Venn
+# Sets em Python são mutáveis, porém aceitam apenas
+# tipos imutáveis como valor interno.
+
+# Criando um set
+# set(iterável) ou {1, 2, 3} # parcem dicionario mas so tem valores 
+# s1 = set('Luiz')
+s1 = set()  # vazio
+s1 = {'Luiz', 1, 2, 3}  # com dados
+
+# Sets são eficientes para remover valores duplicados
+# de iteráveis.
+# - Não aceitam valores mutáveis;
+# - Seus valores serão sempre únicos;
+# - não tem índexes;
+# - não garantem ordem;
+# - são iteráveis (for, in, not in)
+
+# Métodos úteis:
+# add, update, clear, discard
+
+# Operadores úteis:
+# união | união (union) - Une
+# intersecção & (intersection) - Itens presentes em ambos
+# diferença - Itens presentes apenas no set da esquerda
+# diferença simétrica ^ - Itens que não estão em ambos
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#====================== Peculiaridades do tipo mutável set em Python ===============
+# Sets - Conjuntos em Python (tipo set)
+# Conjuntos são ensinados na matemática
+# https://brasilescola.uol.com.br/matematica/conjunto.htm
+# Representados graficamente pelo diagrama de Venn
+# Sets em Python são mutáveis, porém aceitam apenas
+# tipos imutáveis como valor interno.
+
+# Criando um set
+# set(iterável) ou {1, 2, 3}
+# s1 = set('Luiz')
+# s1 = set()  # vazio
+# s1 = {'Luiz', 1, 2, 3}  # com dados
+
+# Sets são eficientes para remover valores duplicados
+# de iteráveis.
+# - Seus valores serão sempre únicos;
+# - Não aceitam valores mutáveis;
+# - não tem índexes;
+# - não garantem ordem;
+# - são iteráveis (for, in, not in)
+# l1 = [1, 2, 3, 3, 3, 3, 3, 1] # podemos usar 'set' para remover valores iguais 
+# s1 = set(l1)
+# l2 = list(s1) # e convertelo para uma lista e manipulala
+# s1 = {1, 2, 3}
+# print(3 not in s1) => false
+# print(2 in s1) => True
+# for numero in s1:
+#     print(numero)
+
+# Métodos úteis:
+# add, update, clear, discard
+
+# Operadores úteis:
+# união | união (union) - Une
+# intersecção & (intersection) - Itens presentes em ambos
+# diferença - Itens presentes apenas no set da esquerda
+# diferença simétrica ^ - Itens que não estão em ambos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#=================== Métodos úteis do tipo set em Python =============================
+# Métodos úteis:
+# add, update, clear, discard
+s1 = set()
+s1.add('Luiz')
+s1.add(1)
+s1.update(('Olá mundo', 1, 2, 3, 4))
+# s1.clear()
+s1.discard('Olá mundo')
+s1.discard('Luiz')
+print(s1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#===============  Operadores importantes para o tipo set (conjuntos) =====================
+# Operadores úteis:
+# união | união (union) - Une
+# intersecção & (intersection) - Itens presentes em ambos
+# diferença - Itens presentes apenas no set da esquerda
+# diferença simétrica ^ - Itens que não estão em ambos
+s1 = {1, 2, 3}
+s2 = {2, 3, 4}
+s3 = s1 | s2
+s3 = s1 & s2
+s3 = s2 - s1
+s3 = s1 ^ s2
+print(s3)
